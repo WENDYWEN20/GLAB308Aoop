9b0288299ba6c54f842ade9ef10eb6624c09b142
wendywen20 <wenyimei6699@gmail.com>
2024-09-03 22:36:17 -0400

    Part 5 finished

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ const adventurer = {
         name: "Leo",
         type: "Cat"
     }
+
     }
+
 console.log(adventurer.inventory[0])
 
 //As an additional practice exercise, create a loop that logs each item in Robin’s inventory.
 function inventoryLog(){
     adventurer.inventory.forEach((inventory) => console.log(inventory))
 }
-inventoryLog(adventurer.inventory)
+inventoryLog(adventurer)
 
 class Character{
     constructor(name,health, inventory, companion){
@@ -29,26 +31,14 @@ class Character{
             console.log(`${this.name} rolled a ${result}.`)}
 
     static Max_Health=100;
+    //static methods are methods that are associated with the class itself, rather than with any particular instance of the class. 
+
 }
 
 //Static methods are often used to create utility functions that perform actions that are independant of the state of an individual object.
 //Static properties are useful for caches, fixed-configuration values, or other data that does not need to be replicated across each instance of the class's objects.
 
-class Companion extends Character{
-    constructor(type, belongings, name, companion){
-        super(name, companion);
-        this.type=type;
-        this.belongings=belongings
-    }
-}
 
-const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
 class Adventurer extends Character {
     constructor (name, role) {
@@ -64,37 +54,60 @@ class Adventurer extends Character {
       console.log(`${this.name} is scouting ahead...`);
       super.roll();
     }
-    
+
     check(){
-        const roles=["Fighter", "Healer", "Wizard"];
-        console.log(roles)
+        let roles=["Fighter", "Healer", "Wizard"];
     if (roles.includes(this.role)) {console.log(`${this.role} is included in ${roles}`)}
-    if (! roles.includes(this.role)) throw new Error(console.log(`${this.role} is not included in ${roles},u need to try another role`))
+    if (! roles.includes(this.role)) throw new Error(console.log(`${this.role} is not included in ${roles}, you need to try another role`))
    }
 
 }
 
-class AdventurerFactory { 
-    constructor (role) {
-    this.role = role;
-    this.adventurers = [];
-    }
-    generate (name) {
-    const newAdventurer = new Adventurer(name, this.role);
-    this.adventurers.push(newAdventurer);
-    }
-    findByIndex (index) {
-    return this.adventurers[index];
-    }
-    findByName (name) {
-    return this.adventurers.find((a) => a.name === name);
-    }
-   }
-   
-    const Adv1= new Adventurer ('Adv1')
-    Adv1.role='badguy'
-    console.log(Adv1)
-    Adv1.scout()
-    Adv1.check()
+
+const Adv1= new Adventurer ('Adv1')
+Adv1.role='Fighter'
+console.log(Adv1)
+Adv1.scout()
+Adv1.check()
+
+
+class Companion extends Character{
+  constructor(type, belongings, name, companion){
+      super(name, companion);
+      this.type=type;
+      this.belongings=belongings
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+
 
     //Part 5 Gather your Party
+    class AdventurerFactory {  
+        constructor (role) {
+          this.role = role;
+          this.adventurers = [];
+        }
+        generate (name) {
+          const newAdventurer = new Adventurer(name, this.role);
+          this.adventurers.push(newAdventurer);
+        }
+        findByIndex (index) {
+          return this.adventurers[index];
+        }
+        findByName (name) {
+          return this.adventurers.find((a) => a.name === name);
+        }
+      }
+      
+      const healers = new AdventurerFactory("Healer");
+      const newRobin = healers.generate("newRobin");
+      console.log(healers)
+      
